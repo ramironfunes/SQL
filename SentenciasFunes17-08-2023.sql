16397aa1636558b9eca7c9e5f629e87c0a26be3e
Ramiro Nicolas Funes <127463276+RAdevDS@users.noreply.github.com>
2023-08-17 11:12:09 -0300

    Add files via upload

---
+++ SentenciasFunes17-08-2023.sql
@@ -0,0 +1,6 @@
+DROP USER IF EXISTS 'kraken@localhost';
+DROP USER IF EXISTS 'ASkywalker';
+CREATE USER IF NOT EXISTS 'kraken@localhost' IDENTIFIED BY 'ramiro123'; -- Creamos el usuario kraken@localhost
+CREATE USER IF NOT EXISTS 'ASkywalker' IDENTIFIED BY 'casa123'; -- Creamos el usuario ASkywalker
+GRANT SELECT ON fastfood.* TO 'ASkywalker'; -- Le otorgamos el permiso de solo lectura al usuario ASkywalker
+GRANT SELECT,INSERT,UPDATE ON fastfood.* TO 'kraken@localhost'; -- Le otorgamos el permiso de lectura, insercion de datos y modificaciones al usuario kraken@gmail.com
